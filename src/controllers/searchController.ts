9634a1971fe1be4c373c5a44ebb4761a4c827ea1
lipebarba2 <felipecinemkt@gmail.com>
2024-09-19 20:29:39 -0300

    feat page de doação de pets

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,16 @@
 import { Request, Response } from 'express'
+import { Pet } from '../models/pet'
+import { createMenuObject } from '../helpers/createMenuObject'
+
 
 export const search = (req: Request, res: Response) => {
+    let query: string = req.query.q as string;
 
+    let list = Pet.getFromName(query)
+
+    res.render('page/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    } )
 }
